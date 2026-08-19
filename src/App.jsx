cefb6c51e3976fe3bda837143c682867b645fb3a
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-19 12:03:54 +0000

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>SalonePadi AI</h1>
+      <p>Kushe! 🦁 Your AI Padi is coming alive.</p>
+    </div>
+  );
+}
+
+export default App;
