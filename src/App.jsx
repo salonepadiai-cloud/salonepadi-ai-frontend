095814967a5274fc0e108aafbee8a46a10ed9493
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-19 15:19:24 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,23 @@ import {
   Route
 } from "react-router-dom";
 
-import Home from "./pages/Home/Home";
-import Login from "./pages/Login/Login";
-import Signup from "./pages/Signup/Signup";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Chat from "./pages/Chat/Chat";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Signup from "./pages/Signup/Signup.jsx";
+import Dashboard from "./pages/Dashboard/Dashboard.jsx";
+import Chat from "./pages/Chat/Chat.jsx";
 
-import ProtectedRoute from "./auth/ProtectedRoute";
+import ProtectedRoute from "./auth/ProtectedRoute.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
+        <Route path="/" element={<Home />} />
 
-        <Route
-          path="/login"
-          element={<Login />}
-        />
+        <Route path="/login" element={<Login />} />
 
-        <Route
-          path="/signup"
-          element={<Signup />}
-        />
+        <Route path="/signup" element={<Signup />} />
 
         <Route element={<ProtectedRoute />}>
           <Route
