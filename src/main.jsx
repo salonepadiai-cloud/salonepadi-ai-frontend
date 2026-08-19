7d11ef5accb0760011d69c348245c185e0eaae06
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-19 15:18:20 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import App from "./App";
-import { AuthProvider } from "./auth/AuthContext";
-
+import App from "./App.jsx";
+import { AuthProvider } from "./auth/AuthContext.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
