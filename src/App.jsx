c7759ff852ac99201e3246682e8bf3ef2484179b
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-19 13:02:11 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-function App() {
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
+import ProtectedRoute from "./auth/ProtectedRoute";
+
+export default function App() {
   return (
-    <div className="app">
-      <h1>SalonePadi AI</h1>
-      <p>Kushe! 🦁 Your AI Padi is coming alive.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/signup"
+          element={<Signup />}
+        />
+
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path="/dashboard"
+            element={<Dashboard />}
+          />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
