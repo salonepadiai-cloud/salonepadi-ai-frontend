92f2705a93d88828eb48b91643b0a09d6298d64b
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-21 11:46:31 +0000

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
-if (!API_URL) {
-  throw new Error("VITE_API_URL is not configured.");
-}
+const API_URL = import.meta.env.VITE_API_URL || "";
 
 export { API_URL };
