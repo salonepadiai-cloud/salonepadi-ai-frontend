8ed135d2e420205ecdec7101f32ab0f2884d4792
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-25 12:10:19 +0000

    Add files via upload

---
+++ js/pages/chat/project-mode.js
@@ -0,0 +1,18 @@
+/*
+|--------------------------------------------------------------------------
+| PROJECT MODE FEATURE
+|--------------------------------------------------------------------------
+|
+| Reserved for lightweight frontend project state:
+|   - file list
+|   - stack
+|   - recent decisions
+|   - current task
+|
+| This is not a replacement for backend AI memory.
+|--------------------------------------------------------------------------
+*/
+
+export function init(context) {
+  return () => {};
+}
