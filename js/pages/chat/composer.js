20a1d7716bd9258f525f143513c1d8453686cf0d
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-25 11:51:55 +0000

    Add files via upload

---
+++ js/pages/chat/composer.js
@@ -0,0 +1,13 @@
+/*
+|--------------------------------------------------------------------------
+| COMPOSER FEATURE
+|--------------------------------------------------------------------------
+|
+| Future location for slash commands, draft persistence, attachments,
+| voice controls and richer composer behaviour.
+|--------------------------------------------------------------------------
+*/
+
+export function init(context) {
+  return () => {};
+}
