bd07a3be9f17f6e4cbcc7ada550f02786bb79209
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-25 12:01:42 +0000

    Add files via upload

---
+++ js/pages/chat/voice.js
@@ -0,0 +1,18 @@
+/*
+|--------------------------------------------------------------------------
+| LIVE VOICE FEATURE
+|--------------------------------------------------------------------------
+|
+| Reserved for:
+|   - microphone permissions
+|   - speech recognition
+|   - live turn-taking
+|   - automatic speech output
+|
+| Kept isolated so normal text chat never depends on voice support.
+|--------------------------------------------------------------------------
+*/
+
+export function init(context) {
+  return () => {};
+}
