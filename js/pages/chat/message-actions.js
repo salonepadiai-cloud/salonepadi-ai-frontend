48a80634a397c6ea629688442740ddde85b07f64
salonepadiai-cloud <salonepadiai@gmail.com>
2026-08-25 13:20:26 +0000

    Add files via upload

---
+++ js/pages/chat/message-actions.js
@@ -0,0 +1,17 @@
+/*
+|--------------------------------------------------------------------------
+| MESSAGE ACTIONS FEATURE
+|--------------------------------------------------------------------------
+|
+| Reserved for:
+|   - regenerate
+|   - copy entire response
+|   - delete message
+|   - edit user message
+|   - feedback
+|--------------------------------------------------------------------------
+*/
+
+export function init(context) {
+  return () => {};
+}
